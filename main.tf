ffaab4e60d0335235c8772680f3d33147b66c881
cloudnagendra99 <cloudnagendra99@gmail.com>
2024-06-19 02:18:36 +0530

    Added securitygroup resource to use it as module later

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "sg" {
+    name    = var.securitygroup_config.name
+    description = var.securitygroup_config.description
+    vpc_id = var.securitygroup_config.vpc_id
+  
+}
+
+resource "aws_security_group_rule" "sg_rules" {
+    count = length(var.securitygroup_config.rules)
+    security_group_id = aws_security_group.sg.id
+    type = var.aws_security_group_config.rules[count.index].type
+    from_port = var.aws_security_group_config.rules[count.index].from_port
+    to_port = var.aws_security_group_config.rules[count.index].to_port
+    protocol = var.aws_security_group_config.rules[count.index].protocol
+    cidr_blocks = [var.aws_security_group_config.rules[count.index].cidr_block]
+
+    depends_on = [ 
+        aws_security_group.sg
+         ]
+  
+}
